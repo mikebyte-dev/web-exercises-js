04f89caa4c4ac18f3535884faf67ffddd711b5de
mikebyte-dev <mikebytedev@gmail.com>
2025-08-13 00:13:13 -0400

    fix(projects/calculator): hadle the zero, and halde the '.' with the keyboard

--- projects/calculator/src/main.js
+++ projects/calculator/src/main.js
@@ -12,12 +12,10 @@ class Calculator {
   handleNumber(number) {
     // Method to handle all the numbers and decimal point
     // if the user try to add a second decimal point, stop the function
-    if (number === "." && this.currentOperand.includes(".")) return;
-
     if (number === "." && this.currentOperand === "") {
       this.currentOperand = "0.";
       return;
-    }
+    } else if (number === "." && this.currentOperand.includes(".")) return;
 
     if (this.equalResult) {
       this.currentOperand = number;
@@ -151,8 +149,7 @@ const calculator = new Calculator();
 
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    if (parseFloat(btn.id) || btn.id === ".") {
-      // if it's a number
+    if ((btn.id >= "0" && btn.id <= "9") || btn.id === ".") {
       calculator.handleNumber(btn.id);
     } else if (btn.id === "c") {
       calculator.clear();
@@ -162,13 +159,9 @@ btns.forEach((btn) => {
       calculator.backspace();
     } else if (btn.id === "+/-") {
       calculator.negative();
-    } else if (btn.id === "0") {
-      // HACK: handle the number zero, cause the parseFloat() don't take it like a number
-      calculator.handleNumber(btn.id);
     } else if (btn.id === "%") {
       calculator.handlePercentage();
     } else {
-      // if its a operator
       calculator.handleOperator(btn.id);
     }
     inputDisplay.value = calculator.updateScreen();
@@ -177,9 +170,8 @@ btns.forEach((btn) => {
 
 // Add listener of the keys
 document.addEventListener("keydown", (e) => {
-  if (e.key >= "0" && e.key <= "9") {
+  if ((e.key >= "0" && e.key <= "9") || e.key === ".") {
     calculator.handleNumber(e.key);
-    console.log(e.key);
   } else if (e.key === "Escape") {
     calculator.clear();
   } else if (e.key === "=" || e.key === "Enter") {
